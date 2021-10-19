eff19b77ff1931ed1b0837ecdcb4a8e98ce0d480
Adil Arandita <aadilarandita@gmail.com>
2021-10-19 09:38:08 +0700

    Tugas 2  : Base admin template

---
+++ resources/views/table.blade.php
@@ -0,0 +1,14 @@
+@extends('adminapp.master')
+
+@section('title', 'Adil table')
+
+@section('content')
+    <div class="content-wrapper">
+
+
+    <div class="container-fluid">
+
+    <h1>Table</h1>
+    </div>
+    </div>
+@endsection
